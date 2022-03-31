2152767f2ec9a3c14da60d9a52a2919899398635
sungjun kim <dd8873@naver.com>
2022-03-31 16:58:57 +0900

    test

---
+++ src/main/java/BlackjackApplication.java
@@ -0,0 +1,8 @@
+import controller.BlackjackManager;
+
+public class BlackjackApplication {
+
+    public static void main(String[] args) {
+        BlackjackManager blackjackManager = new BlackjackManager();
+    }
+}
---
+++ src/main/java/view/Input.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String inputUserName() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세. (쉼표 기준으로 분리)");
+        return SCANNER.nextLine();
+    }
+
+    public static String isCardAdd() {
+        System.out.println(" 는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n)");
+        return SCANNER.nextLine();
+    }
+
+}
