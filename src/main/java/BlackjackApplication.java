7d57bc0c283aab27240adab1ee7ba73586f77f67
sungjun kim <dd8873@naver.com>
2022-04-28 23:50:48 +0900

    Test

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -1,7 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class BlackjackApplication {
 
     public static void main(String[] args) {
-
+        List<Integer> list = new LinkedList<>();
+        list.add(3, 10);
     }
 
 }
