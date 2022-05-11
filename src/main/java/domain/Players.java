5bb2c2d4bf6536b3ad827c8f33cc9a7c0601fddf
sungjun kim <dd8873@naver.com>
2022-05-11 23:39:58 +0900

    players

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,4 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Players {
+
+    private List<Player> players;
+
+    public Players() {
+        this.players = new ArrayList<>();
+    }
+
+    public List<Player> getPlayers() {
+        return this.players;
+    }
 }
