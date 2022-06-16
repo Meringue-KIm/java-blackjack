3c9a9c611656b7391c1c255271968cedffc1d497
sungjun kim <dd8873@naver.com>
2022-06-16 19:06:42 +0900

    수정

--- src/main/java/BlackjackApplication.java
+++ src/main/java/BlackjackApplication.java
@@ -3,6 +3,7 @@ import controller.BlackjackManager;
 public class BlackjackApplication {
 
     public static void main(String[] args) {
+
         try {
             BlackjackManager blackjackManager = new BlackjackManager();
             blackjackManager.Proceed();
